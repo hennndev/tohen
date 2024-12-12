3484a4ce59d5d8b41023ac94e3c3b171fca60ac9
hennndev <hendraputra230897@gmail.com>
2024-12-12 12:19:51 +0700

    fixing bug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+import commonjs from 'vite-plugin-commonjs';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [commonjs(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
