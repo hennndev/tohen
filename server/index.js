6bccc78ea0b9054d2eac59049a209f42cf677864
hennndev <hendraputra230897@gmail.com>
2024-12-12 12:02:43 +0700

    fixing bug

--- server/index.js
+++ server/index.js
@@ -29,7 +29,3 @@ app.use(productRoute)
 app.use(authRoute)
 app.use(userRoute)
 app.use(paymentRoute)
-app.use(express.static(path.join(__dirname, 'dist')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
