7371d138b507a730e1e953265c905f021a5d86c1
hennndev <hendraputra230897@gmail.com>
2024-12-12 11:53:14 +0700

    fixing bug

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const corsOptions = require('./config/corsOptions')
+const path = require('path')
 const app = express()
 require('dotenv').config()
 // routes
